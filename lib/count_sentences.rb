bcb3810372f5a00b28fc3fb35300a422e0aecfaf
jules blanc <jules.blanc@unccub.com>
2019-10-08 21:47:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else
+      return false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def count_sentences
-
-  end
+    counter = 0 
+    self.split(" ").each do |word|
+      if word.sentence? || word.question? || word.exclamation?
+        counter += 1
+      end
+    end
+    return counter
+  end 
 end
