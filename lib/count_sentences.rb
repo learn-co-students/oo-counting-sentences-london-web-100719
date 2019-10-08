3742fe0ca63a71e7a579dde0e52adbd519c051c5
gortron <gordon.lanza@gmail.com>
2019-10-08 16:23:50 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  # Counting sentences isn't easy! We'll use RegExp .split(/.../), which lets us use multiple delimiters. It won't handle all edge cases.
   def count_sentences
-
+    split_sentences = self.split(/[!.?]/)
+    if split_sentences.include?("")
+      split_sentences -= [""]
+    end
+    split_sentences.count
   end
 end
