7abeb46e5542c05bba9987c5bf3225cb48ecc50b
Liko <li_xie1992@hotmail.com>
2019-10-08 18:39:49 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,18 +15,17 @@ class String
   end
 
   def count_sentences
-    # if there is a space after a series of . ? or ! and if there is a character before the first . ? or ! count the sentence
-    #split with . or ? or ! and if the length is greater than 1, it is probably some kind of sentence
-    count = 0
+
+    sentence_count = 0
     minimum_char_length_of_sentece = 1
     split_array = self.split(/[!.?]/)
 
-    split_array.each do | potential_sentence| 
+    split_array.each do | potential_sentence | 
       if potential_sentence.length.to_i  > minimum_char_length_of_sentece
-        count += 1
+        sentence_count += 1
       end
     end
 
-    return count
+    return sentence_count
   end
 end
