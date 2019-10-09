235f84c8519175cd106a4a55f02ea8b16ca83ba8
geewey <geewey@gmail.com>
2019-10-09 19:24:51 +0100

    Update count_sentences.rb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,5 @@ class String
   end
 
   def count_sentences
-    sentence_array = self.split( "." | "?" || "!")
-    sentence_array.select {|item| item.length > 0}.count
+    self.split("." || "?" || "!").select {|item| item.length > 0}.count
 end
