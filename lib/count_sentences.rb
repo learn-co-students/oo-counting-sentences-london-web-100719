0a0e1991c39781f30f4a3e80b398408914bc9c8d
Peter Sampson <peter.sampson010@gmail.com>
2019-10-09 09:27:09 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimeters = ['.', '!', '?']
+    array = self.split(Regexp.union(delimeters))
+    new_array = array.reject {|i| i.empty?}
+    new_array.length
   end
 end
