01b6d4c5fbd210a5b97bc9e38e1d1d7ab0e07942
gortron <gordon.lanza@gmail.com>
2019-10-10 15:32:37 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,12 +14,7 @@ class String
     self.end_with?("!")
   end
 
-  # Counting sentences isn't easy! We'll use RegExp .split(/.../), which lets us use multiple delimiters. It won't handle all edge cases.
   def count_sentences
-    split_sentences = self.split(/[!.?]/)
-    if split_sentences.include?("")
-      split_sentences -= [""]
-    end
-    split_sentences.count
+    self.split.count { |element| element.sentence? || element.question? || element.exclamation? }
   end
 end
